5246b660d87d789f2b8b1ee36cba2b017711456f
Fahmi Aziz <fahmiaziz031102@gmail.com>
2024-08-31 00:42:25 +0700

    update route

--- src/routes/employeeRoute.ts
+++ src/routes/employeeRoute.ts
@@ -12,7 +12,7 @@ router.patch('/userEmployee/:id', verifyToken, updateUserAndEmployee);
 router.delete('/userEmployee/:id', verifyToken, deleteUserAndEmployee);
 router.get('/', getAllEmployees);
 router.get('/:id', getEmployeeById);
-router.put('/:id', updateEmployee);
+router.patch('/:id', updateEmployee);
 router.delete('/:id', deleteEmployee);
 
 export default router;
